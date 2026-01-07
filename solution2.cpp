2b64b5e5d8a3df596513646b6d2d089ecc562377
csh8696-git <csh8696@gmail.com>
2026-01-07 10:09:18 +0900

    solution2_32/3_33
    내적/약수의 개수와 덧셈

---
+++ solution2.cpp
@@ -0,0 +1,21 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    int answer = 0;
+    for (int i = 0; i < a.size(); i++)
+    {
+        answer += a[i] * b[i];
+    }
+    return answer;
+}
+
+#include <vector>
+#include <numeric>
+using namespace std;
+
+int solution(vector<int> a, vector<int> b) {
+    return inner_product(a.begin(), a.end(), b.begin(), 0);
+}
---
+++ solution3.cpp
@@ -0,0 +1,35 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int left, int right) {
+	int answer = 0;
+	int num = 0;
+	for (int i = left; i <= right; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (i % j == 0)
+			{
+				num += 1;
+			}
+		}
+		if (num % 2 == 0)//약수 갯수가 짝수면
+		{
+			answer += i;
+		}
+		else
+		{
+			answer -= i;
+		}
+		num = 0;
+	}
+	return answer;
+}
+
+int sign(int n, int count = 1) {
+	for (int i = 1, last = n >> 1; i <= last; ++i) if (n % i == 0) ++count;
+	return count & 1 ? -1 : 1;
+}
+int solution(int a, int b) { return a > b ? 0 : sign(a) * a + solution(a + 1, b); }
